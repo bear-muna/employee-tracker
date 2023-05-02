78ea10174ab79a5d528d42ed8df9873e236dde09
bear-muna <bearmuna@gmail.com>
2023-05-02 01:16:34 -0700

    deleted queries sql file

--- db/queries.sql
+++
@@ -1,7 +0,0 @@
-USE work_db;
-
-SELECT * FROM department;
-
-SELECT role.title, role.salary FROM role JOIN department ON role.department_id = department.id WHERE department.id = 1;
-
-SELECT * FROM employee;
